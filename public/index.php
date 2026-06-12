7902968cc222f87cab026c6c2d70e8fc483199b8
respaldoisomigbreu-cmd <respaldo.iso.migbreu@gmail.com>
2026-06-11 20:44:04 -0400

    Actualizar controlador de login

--- public/index.php
+++ public/index.php
@@ -2,32 +2,32 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
-use Controllers\loginController;
+use Controllers\LoginController;
 use Controllers\DashboardController;
 use Controllers\TareaControllers;
 use MVC\Router;
 $router = new Router();
 
 // Login
-$router->get('/', [loginController::class, 'login']);
-$router->post('/', [loginController::class, 'login']);
-$router->get('/logout', [loginController::class, 'logout']);
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
 
 //crear cuenta
-$router->get('/crear', [loginController::class, 'crear']);
-$router->post('/crear', [loginController::class, 'crear']);
+$router->get('/crear', [LoginController::class, 'crear']);
+$router->post('/crear', [LoginController::class, 'crear']);
 
 //formulario olvide mi password
-$router->get('/olvide', [loginController::class, 'olvide']);
-$router->post('/olvide', [loginController::class, 'olvide']);
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
 
 //Restablecer mi password
-$router->get('/reestablecer', [loginController::class, 'reestablecer']);
-$router->post('/reestablecer', [loginController::class, 'reestablecer']);
+$router->get('/reestablecer', [LoginController::class, 'reestablecer']);
+$router->post('/reestablecer', [LoginController::class, 'reestablecer']);
 
 //confirmas cuenta
-$router->get('/mensaje', [loginController::class, 'mensaje']);
-$router->get('/confirmar', [loginController::class, 'confirmar']);
+$router->get('/mensaje', [LoginController::class, 'mensaje']);
+$router->get('/confirmar', [LoginController::class, 'confirmar']);
 
 //ZONA PRIVADA DE PROYECTOS
 $router->get('/dashboard', [DashboardController::class, 'index']);
